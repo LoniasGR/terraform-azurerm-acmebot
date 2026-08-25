a358edcf66e2200b1f9bebe88fae31090fc839ee
Leonidas Avdelas <avdelasleonidas@gmail.com>
2026-08-25 12:52:20 +0300

    Try merging the variables instead of null
    Signed-off-by: Leonidas Avdelas <avdelasleonidas@gmail.com>

--- locals.tf
+++ locals.tf
@@ -198,16 +198,18 @@ locals {
     redirect_to_provider          = var.auth_settings.default_provider
     unauthenticated_client_action = "RedirectToLoginPage"
     identity_providers = {
-      azure_active_directory = (var.auth_settings.active_directory != null && var.auth_settings.active_directory.enabled) ? {
-        enabled = true
-        registration = {
-          client_id                  = var.auth_settings.active_directory.client_id
-          client_secret_setting_name = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
-          open_id_issuer             = var.auth_settings.active_directory.tenant_auth_endpoint
-        }
-        } : {
-        enabled = false
-      }
+      azure_active_directory = merge(
+        {
+          enabled = var.auth_settings.active_directory != null && var.auth_settings.active_directory.enabled
+        },
+        (var.auth_settings.active_directory != null && var.auth_settings.active_directory.enabled) ? {
+          registration = {
+            client_id                  = var.auth_settings.active_directory.client_id
+            client_secret_setting_name = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
+            open_id_issuer             = var.auth_settings.active_directory.tenant_auth_endpoint
+          }
+        } : {},
+      )
       custom_open_id_connect_providers = {
         for provider in var.auth_settings.custom_open_id_connect_providers : provider.name => {
           enabled = true
